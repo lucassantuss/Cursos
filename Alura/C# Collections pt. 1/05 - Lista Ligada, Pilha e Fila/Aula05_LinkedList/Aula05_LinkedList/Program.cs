f4665aa23a535102b4fefc735bfa8e3d06493dbb
Lucas Araújo dos Santos <lucassantuss@outlook.com>
2023-12-12 18:18:36 -0300

    11_12_2023 - C# Collections pt. 1

---
+++ Alura/C# Collections pt. 1/05 - Lista Ligada, Pilha e Fila/Aula05_LinkedList/Aula05_LinkedList/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula05_LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Imagine uma lista de frutas
+            List<string> frutas = new List<string>()
+            {
+                "abacate", "banana", "caqui", "damasco", "figo"
+            };
+
+            foreach (var fruta in frutas)
+                Console.WriteLine(fruta);
+
+            // Adicionar um elemento no final de uma lista é rápido
+            // Porém inserir no meio da lista exige mais esforço computacional
+            // Pois ao inserir ou remover no meio da lista, os valores de cada campo tem que se realocar
+
+            // para isso temos a LINKED LIST
+            // -> Elementos não precisam estar em sequência na memória
+            // -> Cada elemento é um "nó" que contém um valor
+            // -> É armazenado a próxima posição e a anterior
+            // -> É utilizado ponteiros
+
+            LinkedList<string> dias = new LinkedList<string>();
+            var d4 = dias.AddFirst("quarta");
+
+            // Cada elemento é um nó: LinkedListNode<T>
+            Console.WriteLine();
+            Console.WriteLine("d4.Value: " + d4.Value);
+
+            // LinkedList não possui único método Add
+            // Possui 4 opções:
+            // 1. Como primeiro nó - AddFirst
+            // 2. Como último nó - AddLast
+            // 3. Antes de um nó conhecido - AddBefore
+            // 4. Depois de um nó conhecido - AddAfter
+            var d2 = dias.AddBefore(d4, "segunda");
+
+            // agora d2 e d4 estão ligados
+            // -> d2.Next é igual a d4
+            // -> d4.Previous é igual a d2
+            var d3 = dias.AddAfter(d2, "terça");
+            var d6 = dias.AddAfter(d4, "sexta");
+            var d7 = dias.AddAfter(d6, "sábado");
+            var d5 = dias.AddBefore(d6, "quinta");
+            var d1 = dias.AddBefore(d2, "domingo");
+
+            // imprimir a lista ligada
+            Console.WriteLine();
+            foreach (var dia in dias)
+                Console.WriteLine(dia);
+
+            // LinkedList NÃO DÁ suporte ao acesso de índice: dias[0]
+            // por isso podemos fazer um laço foreach mas não um for!
+            var quarta = dias.Find("quarta");
+
+            // Para removermos um elemento, podemos tanto remover
+            // pelo valor quanto pela referência do LinkedListNode
+            // dias.Remove("quarta") ou dias.Remove(d4)
+            dias.Remove("quarta");
+
+            Console.WriteLine();
+            foreach (var dia in dias)
+                Console.WriteLine(dia);
+        }
+    }
+}
---
+++ Alura/C# Collections pt. 1/05 - Lista Ligada, Pilha e Fila/Aula05_Stack/Aula05_Stack/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aula05_Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tratando um problema real da navegação de um navegador WEB
+            // Botão voltar que mantém o histórico das páginas anteriores
+            // Botão avançar que mantém o histórico das páginas seguintes
+
+            // PILHA (Stack)
+            var navegador = new Navegador();
+            navegador.NavegarPara("google.com");
+            navegador.NavegarPara("caelum.com.br");
+            navegador.NavegarPara("alura.com.br");
+
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+
+            navegador.Proximo();
+            navegador.Proximo();
+            navegador.Proximo();
+        }
+    }
+
+    internal class Navegador
+    {
+        private readonly Stack<string> historicoAnterior = new Stack<string>();
+        private readonly Stack<string> historicoProximo = new Stack<string>();
+        private string atual = "vazia";
+
+        public Navegador()
+        {
+            Console.WriteLine("Página atual: " + atual);
+        }
+
+        internal void Anterior()
+        {
+            if (historicoAnterior.Any())
+            {
+                historicoProximo.Push(atual);
+                atual = historicoAnterior.Pop();
+                Console.WriteLine("Páginal atual: " + atual);
+            }
+        }
+
+        internal void Proximo()
+        {
+            if (historicoProximo.Any())
+            {
+                historicoAnterior.Push(atual);
+                atual = historicoProximo.Pop();
+                Console.WriteLine("Páginal atual: " + atual);
+            }
+        }
+
+        internal void NavegarPara(string url)
+        {
+            historicoAnterior.Push(atual);
+            atual = url;
+            Console.WriteLine("Páginal atual: " + atual);
+        }
+    }
+}
