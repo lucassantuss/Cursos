d033ec10544cfc82917ab40cdfaf017ba2d29b9b
Lucas Araújo dos Santos <lucassantuss@outlook.com>
2023-12-14 19:15:54 -0300

    14_12_2023 - C# Collections pt. 2

---
+++ Alura/C# Collections pt. 2/05 - Convertendo e Enumerando Coleções/Aula05_Covariancia/Aula05_Covariancia/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula05_Covariancia
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // string herda de Object
+            Console.WriteLine("-> string para object ?");
+
+            string titulo = "meses";
+            object obj = titulo;
+
+            Console.WriteLine(obj);
+            Console.WriteLine();
+
+            Console.WriteLine("-> List<string> para List<object> ?");
+
+            IList<string> listaMeses = new List<string>
+            {
+                "Janeiro", "Fevereiro", "Março",
+                "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro"
+            };
+
+            // não é possível realizar
+            // IList<object> listaObj = listaMeses;
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("-> string[] para object[] ?");
+            
+            string[] arrayMeses = new string[]{
+                "Janeiro", "Fevereiro", "Março",
+                "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro"
+            };
+
+            object[] arrayObj = arrayMeses; // COVARIÂNCIA
+
+            Console.WriteLine(arrayObj);
+            Console.WriteLine();
+
+            foreach (var item in arrayObj)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            arrayObj[0] = "PRIMEIRO MÊS";
+            foreach (var item in arrayObj)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            //arrayObj[0] = 12345;
+            //foreach (var item in arrayObj)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Console.WriteLine("-> List<string> para IEnumerable<object> ?");
+            IEnumerable<object> enumObj = listaMeses; // COVARIÂNCIA SEGURA devido o OUT no parametro de IEnumerable
+            foreach (var item in enumObj)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            //enumObj[0] = 12345;
+        }
+    }
+}
---
+++ Alura/C# Collections pt. 2/05 - Convertendo e Enumerando Coleções/Aula05_Foreach/Aula05_Foreach/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula05_Foreach
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // var meses = new List<string>
+            var meses = new string[]
+            {
+                "Janeiro", "Fevereiro", "Março",
+                "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro",
+            };
+
+            foreach (var mes in meses)
+            {
+                // não pode modificar a variável de iteração do foreach
+                // mes = mes.ToUpper();
+
+                // é gerado um erro se modificar a coleção que está sendo tratada no foreach
+                // Collection was modified; enumeration operation may not execute.
+                // considerando List<string> meses
+                // meses[1] = meses[1].ToUpper();
+
+                // utilizando array não dá erro, porque o laço foreach trabala com array diferente
+                // não trabalha com iterador
+                // transforma como se fosse num laço for
+                meses[1] = meses[1].ToUpper();
+
+                Console.WriteLine(mes);
+            }
+
+            // foreach tratando array é nesse modelo a tratativa por debaixo dos panos
+            for (int i = 0; i < meses.Length; i++)
+            {
+                var mes = meses[i];
+                // . . . . . . . .
+            }
+
+            // Enumerable implementado no método foreach
+
+            // public struct Enumerator : IEnumerator<T>, IEnumerator, IDisposable
+            // {
+            //      Gets the element at the current position of the enumerator.
+            //      public T Current { get; }
+
+            //      Releases all resources used by the System.Collections.Generic.List`1.Enumerator.
+            //      public void Dispose();
+
+            //      Advances the enumerator to the next element of the System.Collections.Generic.List`1.
+            //      public bool MoveNext();
+            // }
+        }
+    }
+}
