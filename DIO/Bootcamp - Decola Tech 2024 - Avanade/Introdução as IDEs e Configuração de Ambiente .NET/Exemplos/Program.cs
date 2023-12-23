cdab9db36677f8a0a059806dece64b8e423b9f1f
Lucas Araújo dos Santos <lucassantuss@outlook.com>
2023-12-23 06:43:25 -0300

    15_12_2023 - Bootcamp Decola Tech DIO

---
+++ DIO/Bootcamp - Decola Tech 2024 - Avanade/Introdução as IDEs e Configuração de Ambiente .NET/Exemplos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exemplos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // No console do VSCode
+            // -> dotnet new console
+
+            // Ir em Extensions e procurar C#
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Exemplo");
+
+            // Outras extensões
+            // C# Extensions do JosKreativ (Menu > New C# > Controller, Class, Enum)
+            // vscode-icons do VSCode Icons Team 
+        }
+    }
+}
